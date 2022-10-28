9650fb417960328bdea48506209ea1407ed4f91e
mmx369 <maksim@klishevich.com>
2022-10-28 13:12:11 +0300

    fix response to async

--- app.js
+++ app.js
@@ -7,28 +7,29 @@ const PORT = 4000
 app.use(express.json())
 
 function sendMail(email, msg) {
-  sendmail(
-    {
-      from: 'test@test.com',
-      to: email,
-      replyTo: 'test@test.com',
-      subject: `Hello ${msg.username}`,
-      html: `Hello ${msg.username}. Otp = ${msg.otp}. `,
-    },
-    function (err, reply) {
-      if (err) {
-        console.log(err && err.stack)
-      } else {
-        console.dir(reply)
+  return new Promise((resolve, reject) => {
+    sendmail(
+      {
+        from: 'test@test.com',
+        to: email,
+        replyTo: 'test@test.com',
+        subject: `Hello ${msg.username}`,
+        html: `Hello ${msg.username}. Otp = ${msg.otp}. `,
+      },
+      function (err, reply) {
+        if (err) {
+          reject(err && err.stack)
+        } else {
+          resolve(reply)
+        }
       }
-    }
-  )
+    )
+  })
 }
 
-app.post('/sendmail', (req, res) => {
+app.post('/sendmail', async (req, res) => {
   const { email, msg } = req.body
-  sendMail(email, msg)
-  console.log(888888, `Email Sent Successfully to ${email}`)
+  await sendMail(email, msg)
   res.send(`Email Sent Successfully to ${email}`)
 })
 
